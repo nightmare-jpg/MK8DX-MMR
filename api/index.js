4b42292d2835bcff5075f34fc134b8e8d02510d8
night <74288636+nightmare-jpg@users.noreply.github.com>
2024-02-08 23:44:53 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,45 +1,32 @@
-const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const app = express();
-const port = 3000; // You can change the port number as needed
+module.exports = async (req, res) => {
+  const { id } = req.query;
 
-app.get('/', (req, res) => {
-    res.send('Hello, your server is running!');
-  });
-  
-  app.get('/mmr', (req, res) => {
-    const { id } = req.query; // Get the 'id' parameter from the query string
-  
-    if (!id) {
-      return res.status(400).json({ error: 'Missing id parameter' });
-    }
-  
-    const url = `https://www.mk8dx-lounge.com/PlayerDetails/${id}`;
-  
-    axios.get(url)
-      .then(response => {
-        const $ = cheerio.load(response.data);
-        const targetDivs = $('div.col-lg-3.col-md-4.col-sm-6.col-xs-6');
-        const result = [];
-  
-        targetDivs.each((index, divElement) => {
-          const dtElement = $(divElement).find('dt');
-          if (dtElement.text().trim() === 'MMR') {
-            const ddElement = $(divElement).find('dd.rank-Iron');
-            result.push(ddElement.text().trim());
-          }
-        });
-  
-        res.json({result});
-      })
-      .catch(error => {
-        console.error('Error fetching HTML page:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      });
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  if (!id) {
+    return res.status(400).json({ error: 'Missing id parameter' });
+  }
+
+  const url = `https://www.mk8dx-lounge.com/PlayerDetails/${id}`;
+
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const targetDivs = $('div.col-lg-3.col-md-4.col-sm-6.col-xs-6');
+    const result = [];
+
+    targetDivs.each((index, divElement) => {
+      const dtElement = $(divElement).find('dt');
+      if (dtElement.text().trim() === 'MMR') {
+        const ddElement = $(divElement).find('dd.rank-Iron');
+        result.push(ddElement.text().trim());
+      }
+    });
+
+    res.json({ mmrValues: result });
+  } catch (error) {
+    console.error('Error fetching HTML page:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
